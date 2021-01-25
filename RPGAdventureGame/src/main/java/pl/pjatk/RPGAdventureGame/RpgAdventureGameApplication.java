ffc10a54a38347f015673e879cda0be2a6547d32
Patryk Klimek <s20158@pjwstk.edu.pl>
2021-01-25 17:13:43 +0100

    Empty project of RPG Adventure game in Spring

---
+++ RPGAdventureGame/src/main/java/pl/pjatk/RPGAdventureGame/RpgAdventureGameApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.RPGAdventureGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpgAdventureGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpgAdventureGameApplication.class, args);
+	}
+
+}
